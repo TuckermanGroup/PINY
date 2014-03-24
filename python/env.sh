d506de20f8bcfbf7a055caee82c677d7aa6bef69
Ondrej Marsalek <ondrej.marsalek@gmail.com>
2014-03-24 11:31:36 -0400

    Fix one more path after build update

--- python/env.sh
+++ python/env.sh
@@ -2,6 +2,6 @@ ENV_BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export PATH=$ENV_BASE_DIR/bin:$PATH
 export PYTHONPATH=$ENV_BASE_DIR:$PYTHONPATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ENV_BASE_DIR"/../runable"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ENV_BASE_DIR"/../lib"
 
 unset ENV_BASE_DIR
