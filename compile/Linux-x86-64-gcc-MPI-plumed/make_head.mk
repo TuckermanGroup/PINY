3dba62e5b3c4708889b4931802d2a4ef716b05d5
Ondrej Marsalek <ondrej.marsalek@gmail.com>
2014-05-23 16:13:42 -0400

    Use the build directory directly, no need to install

--- compile/Linux-x86-64-gcc-MPI-plumed/make_head.mk
+++ compile/Linux-x86-64-gcc-MPI-plumed/make_head.mk
@@ -3,7 +3,7 @@ EXE_NAME = piny-plumed-mpi
 LIB_NAME = libpiny.so
 
 # compilers and options
-PLUMED = /home/om15/opt/plumed-2.0.2
+PLUMED = /home/om15/build/plumed-2.0.2
 FC = mpif77
 CC = mpicc
 DEFINE = -DPLUMED
@@ -11,9 +11,9 @@ DEFINE = -DPLUMED
 OPT = -O2
 OPT_CARE = -O2
 OPT_GRP = -O2
-CFLAGS = -fPIC $(DEFINE) -I $(PLUMED)/include/plumed/wrapper
+CFLAGS = -fPIC $(DEFINE) -I $(PLUMED)/src/wrapper -g
 FFLAGS = -fPIC $(DEFINE)
-LIBS = -L $(PLUMED)/lib -lplumed -ldl -Wl,-rpath,$(PLUMED)/lib
+LIBS = -L $(PLUMED)/src/lib -lplumed -ldl -Wl,-rpath,$(PLUMED)/src/lib
 
 BASE = $(realpath ../..)
 CODE = $(BASE)/src
